57ffc041b6fdc14d40f0f38b9f2ea12b9ad3fd1b
Aniket <aniketninama5@gmail.com>
2025-08-21 16:53:25 +0530

    static files 7 commit

--- Weebwatchlist/settings.py
+++ Weebwatchlist/settings.py
@@ -30,7 +30,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
