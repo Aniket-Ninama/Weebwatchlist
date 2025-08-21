4e01a3fe69b7fb54a8f9933efb1c0e4a87d0c6ee
Aniket <aniketninama5@gmail.com>
2025-08-21 15:30:54 +0530

    static files 4 commit

--- Weebwatchlist/settings.py
+++ Weebwatchlist/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
     BASE_DIR / "theme" / "static",
 ]
 
