0336692dec980f400d5e071275b81a3fd29f0425
Aniket <aniketninama5@gmail.com>
2025-08-21 15:10:40 +0530

    static files 3 commit

--- Weebwatchlist/settings.py
+++ Weebwatchlist/settings.py
@@ -141,5 +141,5 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 TIME_ZONE = 'Asia/Kolkata'
