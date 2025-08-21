7e77a0b23d08217f6ab5b963e820747ab163bb83
Aniket <aniketninama5@gmail.com>
2025-08-21 14:33:29 +0530

    tenth commit

--- start.sh
+++ start.sh
@@ -4,8 +4,12 @@ python manage.py migrate
 
 # Create superuser if it doesn't exist
 python - <<END
-from django.contrib.auth.models import User
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Weebwatchlist.settings')  # <--- add this
+import django
+django.setup()
+
+from django.contrib.auth.models import User
 
 username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
@@ -17,4 +21,3 @@ END
 
 # Start Gunicorn
 gunicorn Weebwatchlist.wsgi:application --bind 0.0.0.0:8000 --workers 3
-
